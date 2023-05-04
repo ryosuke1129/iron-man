67769bea66bfffa648577c3c1b09b62a11d678be
Ryosuke <ryosuke.kanasugi@gmail.com>
2023-05-05 02:56:32 +0900

    update

---
+++ lambda_function.py
@@ -0,0 +1,70 @@
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage
+from boto3.dynamodb.conditions import Key, Attr
+import boto3
+import datetime
+import json
+import openai
+import os
+
+LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+OPENAI_ORGANIZATION = os.getenv('OPENAI_ORGANIZATION')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('iron-man')
+
+def chat_completion(text, user_id):
+    openai.organization = OPENAI_ORGANIZATION
+    openai.api_key = OPENAI_API_KEY
+    openai.Model.list()
+    config = '''
+    あなたはアイアンマンです。アイアンマンのように男らしい口調で会話をしてください。
+    以下の会話履歴に沿った返答をしてください。
+    ----------\n
+    '''
+    response = table.query(
+        KeyConditionExpression=Key('user_id').eq(user_id)
+    )
+    items = response['Items']
+    for item in items:
+        config += item['user_content']+'\n'
+        config += item['GPT_reply']+'\n----------\n'
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": config + text + '。'}]
+        )
+    return completion['choices'][0]['message']['content']
+
+def lambda_handler(event, context):
+    message_id = str(json.loads(event['body'])['events'][0]['message']['id'])
+    reply_token = json.loads(event['body'])['events'][0]['replyToken']
+    user_id = json.loads(event['body'])['events'][0]['source']['userId']
+    message = json.loads(event['body'])['events'][0]['message']['text']
+    if not json.loads(event['body'])['events'][0]['message']['type'] == 'text':
+        line_bot_api.reply_message(reply_token, TextSendMessage(text='今は会話しかできないんだ。言葉を交わそうじゃないか。'))
+    if json.loads(event['body'])['events'][0]['message']['type'] == 'text':
+        try:
+            reply_message = chat_completion(message, user_id)
+            table.put_item(Item={'user_id': user_id,
+                                'send_at': str(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))),
+                                'message_id': message_id,
+                                'user_content': message,
+                                'GPT_reply': reply_message}
+                                )
+            ok_json = {"isBase64Encoded": False,
+                    "statusCode": 200,
+                    "headers": {},
+                    "body": ""}
+            line_bot_api.reply_message(reply_token, TextSendMessage(text=reply_message))
+            return ok_json
+        except:
+            error_json = {"isBase64Encoded": False,
+                        "statusCode": 500,
+                        "headers": {},
+                        "body": "Error"}
+            line_bot_api.reply_message(reply_token, TextSendMessage(text='エラーが発生しました。'))
+            return error_json
